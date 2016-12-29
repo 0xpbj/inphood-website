65035679b650189ff79e039e6f40fbdf3a145e9a
inphood <prabhaav.bhardwaj@gmail.com>
2016-12-28 16:16:13 -0800

    indexing for user data

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -6,16 +6,18 @@ import Col from 'react-bootstrap/lib/Col'
 import Grid from 'react-bootstrap/lib/Grid'
 import Image from 'react-bootstrap/lib/Image'
 import Button from 'react-bootstrap/lib/Button'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 
 export default class User extends React.Component {
   constructor() {
     super()
     this.state = {
-      grid: false
+      grid: false,
+      index: 0
     }
   }
-  toggleGrid() {
-    this.setState({grid: !this.state.grid})
+  toggleGrid(index) {
+    this.setState({grid: !this.state.grid, index})
   }
   render() {
     if (this.props.data.photos.length === 0) {
@@ -41,7 +43,7 @@ export default class User extends React.Component {
             src: img.picture,
             thumbnail: img.thumbnail,
             caption: img.caption.text,
-            tags: img.tags,
+            // tags: img.tags,
             thumbnailWidth: 150,
             thumbnailHeight: 150
           }
@@ -55,10 +57,11 @@ export default class User extends React.Component {
         return (
           <Grid>
             <Row className="show-grid">
-              <Col xs={12} md={8}><Image src={data[0].picture} rounded /></Col>
+              <Col xs={12} md={8}><Image src={data[this.state.index].picture} rounded /></Col>
               <Col xs={6} md={4}>
+                <ControlLabel>Ingredients</ControlLabel>
                 <textarea rows="4" cols="50">
-                  {data[0].caption.text}
+                  {data[this.state.index].caption.text}
                 </textarea>
                 <Button bsStyle="success" onClick={this.toggleGrid.bind(this)}>Done</Button>
               </Col>

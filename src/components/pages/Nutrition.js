4bfc8026615ce37066930c2cee86766c0b473bf8
Alex Carreira <aycarrei@gmail.com>
2017-01-05 20:26:56 -0800

    Update Nutrition page/scene to properly handle different sliders and their state. Integrated their state with rendering of the nutrition label.

--- src/components/pages/Nutrition.js
+++ src/components/pages/Nutrition.js
@@ -10,58 +10,79 @@ export default class Nutrition extends React.Component {
     super(props);
 
     this.state = {
-      value: 100
+      sliderValueDict: {},
+      matches: [],
+      nutAlg: new NutritionAlg()
     };
   }
 
-  handleValuesChange(component, value) {
+  handleSliderValuesChange(component, value) {
+    var sliderValueDict = this.state.sliderValueDict
+    sliderValueDict[component.props.id] = value
     this.setState({
-      value: value,
-    });
+      sliderValueDict: sliderValueDict,
+    })
+  }
+
+  componentWillMount() {
+    // Process the caption for matches in the FDA database:
+    //
+    const tagString = this.props.nutrition.caption
+    // TODO: AC! **************** regexp errors here for / characters
+    this.state.nutAlg.processTags(tagString)
+
+    // Create the slider values dictionary state and initialize each one to 100:
+    //
+    var sliderValueDict = {}
+    for (var tag in this.state.nutAlg.getMatches()) {
+      sliderValueDict[tag] = 100
+    }
+
+    this.setState({
+      matches: this.state.nutAlg.getMatches(),
+      sliderValueDict: sliderValueDict
+    })
   }
 
   render() {
     console.log("Nutrition render() ----------------------------------")
-    console.log(this.props)
 
     // 1. Separate out the content of the photo caption into the following
     //    structure for each ingredient:
     //        ingredient quantity unit nutrition_info
     //        ...
     //        ingredient quantity unit nutrition_info
-    const tagString = this.props.nutrition.caption
-    // AC! **************** regexp errors here for / characters
-    let nutAlg = new NutritionAlg()
-    nutAlg.processTags(tagString)
-    const matches = nutAlg.getMatches()
+
     var sliders = []
     var notFound = ""
     var fat = 0.0
     var carbs = 0.0
     var protein = 0.0
-    for (var tag in matches) {
+    for (var tag in this.state.matches) {
 
-      const bestMatch = nutAlg.getBestMatchForTag(tag)
+      const bestMatch = this.state.nutAlg.getBestMatchForTag(tag)
       if (bestMatch == "") {
         notFound = notFound + tag + " "
         continue
       }
 
-      const dataForKey = nutAlg.getDataForKey(bestMatch)
+      const dataForKey = this.state.nutAlg.getDataForKey(bestMatch)
       // TODO: need a big number (non float limited version to do real math here)
-      fat += parseFloat(dataForKey['Fat'])
-      carbs += parseFloat(dataForKey['Carbohydrate'])
-      protein += parseFloat(dataForKey['Protein'])
-      console.log('Carbs = ' + carbs)
+      fat += parseFloat(dataForKey['Fat'] * this.state.sliderValueDict[tag] / 100)
+      carbs += parseFloat(dataForKey['Carbohydrate'] * this.state.sliderValueDict[tag] / 100)
+      protein += parseFloat(dataForKey['Protein'] * this.state.sliderValueDict[tag] / 100)
+
+      console.log("Value for tag '" + tag + "' = " + this.state.sliderValueDict[tag])
 
       sliders.push(
         <div>
           <text>{bestMatch} (grams)</text>
           <InputRange
+            id={tag}
             maxValue={100}
             minValue={0}
-            value={this.state.value}
-            onChange={this.handleValuesChange.bind(this)}/>
+            value={this.state.sliderValueDict[tag]}
+            onChange={this.handleSliderValuesChange.bind(this)}/>
           <br/><br/>
         </div>)
     }
@@ -75,6 +96,15 @@ export default class Nutrition extends React.Component {
     const carbRDA = 100.0 * carbs / 300.0
     const proteinRDA = 100.0 * protein / 50.0
 
+    const totalFatStr = fat.toFixed(2) + " g"
+    const fatRDAStr = fatRDA.toFixed(2) + "%"
+
+    const totalCarbsStr = carbs.toFixed(2) + " g"
+    const carbRDAStr = carbRDA.toFixed(2) + "%"
+
+    const totalProteinStr = protein.toFixed(2) + " g"
+
+    const tagString = this.props.nutrition.caption
     return (
       <div>
         <Button className="btn-primary-spacing" bsStyle="success" onClick={() => this.props.goToGallery()}>Gallery</Button>
@@ -84,15 +114,15 @@ export default class Nutrition extends React.Component {
         <Label
           servingAmount="100" servingUnit="g"
           totalCal="200" totalFatCal="130"
-          totalFat={fat} totalFatDayPerc={fatRDA}
+          totalFat={totalFatStr} totalFatDayPerc={fatRDAStr}
           saturatedFat="9g" saturatedFatDayPerc="22%"
           transFat="0g"
           cholesterol="55mg" cholesterolDayPerc="80%"
           sodium="40mg" sodiumDayPerc="2%"
-          totalCarb={carbs} totalCarbDayPerc={carbRDA}
+          totalCarb={totalCarbsStr} totalCarbDayPerc={carbRDAStr}
           fiber="1g" fiberDayPerc="4%"
           sugars="14g"
-          protein={protein}/>
+          protein={totalProteinStr}/>
       </div>
     )
   }

50b3297f52fc64aa8e2d416eddaa67c9e0973b96
inphood <prabhaav.bhardwaj@gmail.com>
2017-02-09 22:45:54 -0800

    Home page revamp

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,31 +6,15 @@ import Grid from 'react-bootstrap/lib/Grid'
 import Alert from 'react-bootstrap/lib/Alert'
 import Image from 'react-bootstrap/lib/Image'
 import Button from 'react-bootstrap/lib/Button'
-import MenuItem from 'react-bootstrap/lib/MenuItem'
 import Jumbotron from 'react-bootstrap/lib/Jumbotron'
-import FormGroup from 'react-bootstrap/lib/FormGroup'
-import FormControl from 'react-bootstrap/lib/FormControl'
 import Glyphicon from 'react-bootstrap/lib/Glyphicon'
-import ControlLabel from 'react-bootstrap/lib/ControlLabel'
-import DropdownButton from 'react-bootstrap/lib/DropdownButton'
 import ImageGallery from 'react-image-gallery'
-import {Redirect} from 'react-router'
-import {parseRecipe} from '../../helpers/parseRecipe'
-import Hello from 'hellojs'
-import UploadModal from '../layout/UploadModal'
-
-const Config = require('Config')
-
 import "react-image-gallery/styles/css/image-gallery.css"
+const Config = require('Config')
 
 export default class Home extends React.Component {
   constructor() {
     super()
-    this.state = {
-      url: '',
-      showUploadModal: false,
-      anonymous: false
-    }
   }
   componentWillMount() {
     if (this.props.user.anonymous)
@@ -49,30 +33,6 @@ export default class Home extends React.Component {
       nonInteraction: false
     });
   }
-  handleUrl() {
-    if (this.getValidationState() === 'success') {
-      this.props.anSelectedPhoto(this.state.url)
-      ReactGA.event({
-        category: 'User',
-        action: 'Anonymous flow initiated',
-        label: 'URL Flow',
-        nonInteraction: false
-      });
-      this.props.router.push('image')
-    }
-  }
-  // onDrop(acceptedFiles, rejectedFiles) {
-  //   ReactGA.event({
-  //     category: 'User',
-  //     action: 'Image upload flow initiated',
-  //     label: 'Local Image Flow',
-  //     nonInteraction: false
-  //   });
-  //   if (acceptedFiles.length > 0) {
-  //     this.props.anSelectedPhoto(acceptedFiles[0].preview)
-  //     this.props.router.push('image')
-  //   }
-  // }
   selectPhoto() {
     const index = this._imageGallery.getCurrentIndex()
     const photo =  this.props.user.photos.data[index]
@@ -86,135 +46,47 @@ export default class Home extends React.Component {
     });
     this.props.router.push('image')
   }
-  getValidationState() {
-    const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    const result = urlPattern.test(this.state.url)
-    if (result)
-      return 'success'
-    else {
-      if (this.state.url !== '')
-        return 'error'
-    }
-  }
   render() {
-    if (!this.props.user.login && !this.state.anonymous) {
-      let hideUploadModal = () => this.setState({ showUploadModal: false });
+    if (!this.props.user.login) {
       const inPhoodLogo = require('../../images/logoLarge.jpg')
+      var sectionStyle = {
+        width: '100%',
+        height: '400px',
+        backgroundImage: `url(${inPhoodLogo})`
+      }
       return (
         <div>
-          <div>
-            <Grid>
-              {/*<Row style={{margin: 50}}>
-                <Col xs={12} md={12}>
-                  <img src={inPhoodLogo} className="center-block" alt="Welcome to inPhood!"/>
-                </Col>
-              </Row>*/}
-              <Jumbotron>
-                <h1>What's really in your food?</h1>
-                <p></p>
-                <p><Button bsStyle="primary">Learn more</Button></p>
-              </Jumbotron>
-              <Row>
-                <div className="text-center">
-                  <Col xs={3} md={3}>
-                  </Col>
-                  <Col xs={3} md={3}>
-                    <form onSubmit={this.handleUrl.bind(this)}>
-                      <FormGroup
-                        controlId="formBasicText"
-                        validationState={this.getValidationState()}
-                      >
-                        <FormControl
-                          className="text-center"
-                          type="text"
-                          value={this.state.url}
-                          placeholder="paste image url here"
-                          onChange={(e) => this.setState({url: e.target.value.toLowerCase()})}
-                        />
-                      </FormGroup>
-                    </form>
-                  </Col>
-                  <Col xs={1} md={1} style={{paddingTop: 6}}>
-                    <text>or</text>
-                  </Col>
-                  <Col xs={2} md={2}>
-                    <Button onClick={this.handleClick.bind(this)}>Sign in with Instagram</Button>
-                  </Col>
-                  <Col xs={3} md={3}>
-                  </Col>
-
-                </div>
-              </Row>
-
-              {/* TODO: PBJ, delete this unless you need something from it--I
-                restyled it into the row seen above.*/}
-              {/*<Row>
-                <div className="text-center">
-                  <Col xs={6} md={6}>
-                    <Row>
-                      <Col xs={12} md={12}>
-                        <form onSubmit={this.handleUrl.bind(this)}>
-                          <FormGroup
-                            controlId="formBasicText"
-                            validationState={this.getValidationState()}
-                          >
-                            <FormControl
-                              className="text-center"
-                              type="text"
-                              value={this.state.url}
-                              placeholder="add image url here"
-                              onChange={(e) => this.setState({url: e.target.value.toLowerCase()})}
-                            />
-                          </FormGroup>
-                        </form>
-                      </Col>
-                      <Col xs={1} md={1}>
-                        <Button className="btn-primary-spacing" onClick={this.handleUrl.bind(this)}><Glyphicon glyph="glyphicon glyphicon-search"></Glyphicon></Button>
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col xs={1} md={1}>
-                    <Button bsStyle="default" onClick={()=>this.setState({ showUploadModal: true })}>
-                      <Glyphicon glyph="glyphicon glyphicon-open" />
-                    </Button>
-                    <UploadModal
-                      onDrop={(acceptedFiles, rejectedFiles) => this.onDrop.bind(this)}
-                      show={this.state.showUploadModal}
-                      onHide={hideUploadModal}
-                    />
-                  </Col>
-                  <Col xs={6} md={6}>
-                    <Button onClick={this.handleClick.bind(this)}>Sign in with Instagram</Button>
-                  </Col>
-                  <Button onClick={this.handleClick.bind(this)}>Sign in with Instagram</Button>
-                </div>
-              </Row>
-              */}
-            </Grid>
-          </div>
+          <Grid>
+            <Jumbotron>
+              <h1 className="text-center">What's really in your food?</h1>
+              <h3 className="text-center">Make nutrition labels in three easy steps!</h3>
+              <p className="text-right"><Button bsStyle="primary">Learn more</Button></p>
+            </Jumbotron>
+            {/*<section style={ sectionStyle }></section>*/}
+            <Row>
+              <div className="text-center">
+                <Button onClick={this.handleClick.bind(this)}>Sign in with Instagram</Button>
+              </div>
+            </Row>
+          </Grid>
         </div>
       )
     }
-    else if (this.props.user.error !== '' && !this.state.anonymous) {
+    else if (this.props.user.error !== '') {
       return (
         <Alert bsStyle="danger">
           <strong>Error: {this.props.user.error}</strong>
         </Alert>
       )
     }
-    else if (!this.state.anonymous && (this.props.user.photos.length === 0 || this.props.user.photos.data.length === 0)) {
+    else if (this.props.user.photos.length === 0 || this.props.user.photos.data.length === 0) {
       return (
         <Alert bsStyle="info">
           <strong>Photos loading...</strong>
         </Alert>
       )
     }
-    else if (!this.state.anonymous) {
+    else {
       ReactGA.set({ userId: this.props.user.profile.name })
       ReactGA.event({
         category: 'User',
@@ -246,15 +118,6 @@ export default class Home extends React.Component {
                 <Col xs={12} md={7}>
                   <text><b>Step 1:</b> Select a food image to create a label...</text>
                 </Col>
-                {/*<Col xs={12} md={2}>
-                  <DropdownButton bsStyle="info" title="Options" id={`dropdown-basic`}>
-                    <MenuItem key="1" onClick={() => this.props.igRefreshRequest()}>Refresh</MenuItem>
-                    <MenuItem key="2" onClick={() => this.props.igLogoutRequest()}>Logout</MenuItem>
-                  </DropdownButton>
-                </Col>*/}
-                {/*<Button className="btn-primary-spacing" onClick={() => this.props.igLogoutRequest()}>
-                  <Glyphicon glyph="glyphicon glyphicon-log-out"></Glyphicon>
-                </Button>*/}
                 <Col xs={12} md={3}>
                   <Button className="btn-primary-spacing"
                           bsStyle="success"
@@ -277,7 +140,6 @@ export default class Home extends React.Component {
                 showFullscreenButton={false}
                 showNav={true}
                 showPlayButton={false}
-                onClick={this.selectPhoto.bind(this)}
               />
             </Col>
             <Col xs={2} md={2} />

ea1a4c402b044184b6f306da2c45ecdc818c9abd
Alex Carreira <aycarrei@gmail.com>
2017-03-08 22:52:05 -0800

    Add margin to fix save on Label

--- src/components/pages/NutritionEstimateJSX.js
+++ src/components/pages/NutritionEstimateJSX.js
@@ -62,165 +62,170 @@ export default class NutritionEstimateJSX extends React.Component {
       (<p style={myStyles.performanceFactsHeaderElementP}>{servingSizeSentence}<br/>
         Servings Per Recipe about {ingredientComposite.getSuggestedServingAmount()}</p>)
 
+    // The div-in-div below and margin of 4 is to fix a save image issue (where it
+    // cuts off part of the nutrition label border and the generated at inphood.com
+    // is too low on the image).
     return(
       <div
         id="nutrition-label"
-        style={{backgroundColor:'white'}}>
-        <section style={myStyles.performanceFacts}>
-          <header style={myStyles.performanceFactsHeader}>
-            <h1 style={myStyles.performanceFactsTitle}>Nutrition Facts</h1>
-            {servingSection}
-          </header>
-          <table style={myStyles.performanceFactsTable}>
-            <thead>
-              <tr>
-                <th
-                  colSpan={3}
-                  style={{...myStyles.performanceFactsTableElementTheadTrTh,
-                          ...myStyles.smallInfo}}>
-                  Amount Per Serving
-                </th>
-              </tr>
-            </thead>
-            <tbody>
+        style={{backgroundColor:'white', padding:4}}>
+        <div style={{margin:0}}>
+          <section style={myStyles.performanceFacts}>
+            <header style={myStyles.performanceFactsHeader}>
+              <h1 style={myStyles.performanceFactsTitle}>Nutrition Facts</h1>
+              {servingSection}
+            </header>
+            <table style={myStyles.performanceFactsTable}>
+              <thead>
+                <tr>
+                  <th
+                    colSpan={3}
+                    style={{...myStyles.performanceFactsTableElementTheadTrTh,
+                            ...myStyles.smallInfo}}>
+                    Amount Per Serving
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
 
-              <tr>
-                <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
-                  <b>Calories </b>
-                  {ingredientComposite.getCalories()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  Calories from Fat&nbsp;
-                  {ingredientComposite.getCaloriesFromFat()}
-                </td>
-              </tr>
+                <tr>
+                  <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
+                    <b>Calories </b>
+                    {ingredientComposite.getCalories()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    Calories from Fat&nbsp;
+                    {ingredientComposite.getCaloriesFromFat()}
+                  </td>
+                </tr>
 
-              <tr className="thick-row">
-                <td colSpan={3}
-                  style={{...myStyles.smallInfo,
-                          ...myStyles.performanceFactsTableElementTdLastChild,
-                          ...myStyles.performanceFactsTableClassThickRowTd}}>
-                  <b>% Daily Value*</b>
-                </td>
-              </tr>
+                <tr className="thick-row">
+                  <td colSpan={3}
+                    style={{...myStyles.smallInfo,
+                            ...myStyles.performanceFactsTableElementTdLastChild,
+                            ...myStyles.performanceFactsTableClassThickRowTd}}>
+                    <b>% Daily Value*</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
-                  <b>Total Fat </b>
-                  {ingredientComposite.getTotalFatPerServing()}
-                  {ingredientComposite.getTotalFatUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  <b>{ingredientComposite.getTotalFatRDA()}%</b>
-                </td>
-              </tr>
+                <tr>
+                  <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
+                    <b>Total Fat </b>
+                    {ingredientComposite.getTotalFatPerServing()}
+                    {ingredientComposite.getTotalFatUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    <b>{ingredientComposite.getTotalFatRDA()}%</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <td
-                  style={{...myStyles.performanceFactsTableElementTd,
-                          ...myStyles.performanceFactsTableClassBlankCell}}>
-                </td>
-                <th style={myStyles.performanceFactsTableElementTh}>
-                  Saturated Fat&nbsp;
-                  {ingredientComposite.getSaturatedFatPerServing()}
-                  {ingredientComposite.getSaturatedFatUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  <b>{ingredientComposite.getSaturatedFatRDA()}%</b>
-                </td>
-              </tr>
+                <tr>
+                  <td
+                    style={{...myStyles.performanceFactsTableElementTd,
+                            ...myStyles.performanceFactsTableClassBlankCell}}>
+                  </td>
+                  <th style={myStyles.performanceFactsTableElementTh}>
+                    Saturated Fat&nbsp;
+                    {ingredientComposite.getSaturatedFatPerServing()}
+                    {ingredientComposite.getSaturatedFatUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    <b>{ingredientComposite.getSaturatedFatRDA()}%</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <td
-                  style={{...myStyles.performanceFactsTableElementTd,
-                          ...myStyles.performanceFactsTableClassBlankCell}}>
-                </td>
-                <th style={myStyles.performanceFactsTableElementTh}>
-                  Trans Fat&nbsp;
-                  {ingredientComposite.getTransFatPerServing()}
-                  {ingredientComposite.getTransFatUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                </td>
-              </tr>
+                <tr>
+                  <td
+                    style={{...myStyles.performanceFactsTableElementTd,
+                            ...myStyles.performanceFactsTableClassBlankCell}}>
+                  </td>
+                  <th style={myStyles.performanceFactsTableElementTh}>
+                    Trans Fat&nbsp;
+                    {ingredientComposite.getTransFatPerServing()}
+                    {ingredientComposite.getTransFatUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                  </td>
+                </tr>
 
-              <tr>
-                <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
-                  <b>Cholesterol </b>
-                  {ingredientComposite.getCholestorol()}
-                  {ingredientComposite.getCholestorolUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  <b>{ingredientComposite.getCholestorolRDA()}%</b>
-                </td>
-              </tr>
+                <tr>
+                  <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
+                    <b>Cholesterol </b>
+                    {ingredientComposite.getCholestorol()}
+                    {ingredientComposite.getCholestorolUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    <b>{ingredientComposite.getCholestorolRDA()}%</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
-                  <b>Sodium </b>
-                  {ingredientComposite.getSodium()}
-                  {ingredientComposite.getSodumUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  <b>{ingredientComposite.getSodiumRDA()}%</b>
-                </td>
-              </tr>
+                <tr>
+                  <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
+                    <b>Sodium </b>
+                    {ingredientComposite.getSodium()}
+                    {ingredientComposite.getSodumUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    <b>{ingredientComposite.getSodiumRDA()}%</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
-                  <b>Total Carbohydrate </b>
-                  {ingredientComposite.getTotalCarbohydratePerServing()}
-                  {ingredientComposite.getTotalCarbohydrateUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  <b>{ingredientComposite.getTotalCarbohydrateRDA()}%</b>
-                </td>
-              </tr>
+                <tr>
+                  <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
+                    <b>Total Carbohydrate </b>
+                    {ingredientComposite.getTotalCarbohydratePerServing()}
+                    {ingredientComposite.getTotalCarbohydrateUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    <b>{ingredientComposite.getTotalCarbohydrateRDA()}%</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <td
-                  style={{...myStyles.performanceFactsTableElementTd,
-                          ...myStyles.performanceFactsTableClassBlankCell}}>
-                </td>
-                <th style={myStyles.performanceFactsTableElementTh}>
-                  Dietary Fiber&nbsp;
-                  {ingredientComposite.getDietaryFiber()}
-                  {ingredientComposite.getDietaryFiberUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                  <b>{ingredientComposite.getDietaryFiberRDA()}%</b>
-                </td>
-              </tr>
+                <tr>
+                  <td
+                    style={{...myStyles.performanceFactsTableElementTd,
+                            ...myStyles.performanceFactsTableClassBlankCell}}>
+                  </td>
+                  <th style={myStyles.performanceFactsTableElementTh}>
+                    Dietary Fiber&nbsp;
+                    {ingredientComposite.getDietaryFiber()}
+                    {ingredientComposite.getDietaryFiberUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                    <b>{ingredientComposite.getDietaryFiberRDA()}%</b>
+                  </td>
+                </tr>
 
-              <tr>
-                <td
-                  style={{...myStyles.performanceFactsTableElementTd,
-                          ...myStyles.performanceFactsTableClassBlankCell}}>
-                </td>
-                <th style={myStyles.performanceFactsTableElementTh}>
-                  Sugars&nbsp;
-                  {ingredientComposite.getSugars()}
-                  {ingredientComposite.getSugarsUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                </td>
-              </tr>
+                <tr>
+                  <td
+                    style={{...myStyles.performanceFactsTableElementTd,
+                            ...myStyles.performanceFactsTableClassBlankCell}}>
+                  </td>
+                  <th style={myStyles.performanceFactsTableElementTh}>
+                    Sugars&nbsp;
+                    {ingredientComposite.getSugars()}
+                    {ingredientComposite.getSugarsUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                  </td>
+                </tr>
 
-              <tr style={myStyles.thickEnd}>
-                <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
-                  <b>Protein </b>
-                  {ingredientComposite.getTotalProteinPerServing()}
-                  {ingredientComposite.getTotalProteinUnit()}
-                </th>
-                <td style={myStyles.performanceFactsTableElementTdLastChild}>
-                </td>
-              </tr>
+                <tr style={myStyles.thickEnd}>
+                  <th colSpan={2} style={myStyles.performanceFactsTableElementTh}>
+                    <b>Protein </b>
+                    {ingredientComposite.getTotalProteinPerServing()}
+                    {ingredientComposite.getTotalProteinUnit()}
+                  </th>
+                  <td style={myStyles.performanceFactsTableElementTdLastChild}>
+                  </td>
+                </tr>
 
-            </tbody>
-          </table>
-          <p style={myStyles.smallInfo}>* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:</p>
-        </section>
-        {this.getGeneratedStatement(displayGeneratedStatement)}
+              </tbody>
+            </table>
+            <p style={myStyles.smallInfo}>* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:</p>
+          </section>
+          {this.getGeneratedStatement(displayGeneratedStatement)}
+        </div>
       </div>
     )
   }
